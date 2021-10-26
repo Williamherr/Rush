0b070a24103d52fb14c237535fcf4213b957482a
William Herr <herrwilliam@outlook.com>
2021-10-25 22:01:38 -0400

    Added Navigation Bar

--- app/src/main/java/com/example/rush/MainActivity.java
+++ app/src/main/java/com/example/rush/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.rush;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import static java.lang.String.valueOf;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +21,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigation();
+
+    }
+
+    public void bottomNavigation() {
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationBar);
+        bottomNav.setOnItemSelectedListener(new  NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id){
+                    case 2131231024: //Home
+                        Log.d("navBar","Home");
+                        break;
+                    case 2131231023: //Groups
+                        Log.d("navBar","Groups");
+                        break;
+                    case 2131231211: //Classes
+                        Log.d("navBar","Classes");
+                        break;
+                    case 2131231025: //Messages
+                        Log.d("navBar","Messages");
+                        messageFragment();
+                        break;
+                    case 2131231210: //Activity
+                        Log.d("navBar","Activity");
+                        break;
+                    default:
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+
+
+
+
+    public void messageFragment() {
+        setTitle("Messages");
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerView, new MessageFragment())
+                .commit();
     }
 }
--- app/src/main/java/com/example/rush/MessageFragment.java
+++ app/src/main/java/com/example/rush/MessageFragment.java
@@ -9,21 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class LoginFragment extends Fragment {
+public class MessageFragment extends Fragment {
 
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public MessageFragment() {
+        // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
 
 
         return view;
