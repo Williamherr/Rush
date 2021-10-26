92766bbaedc358370218351bba1d82f3bcf2ff9e
AndyLeUNCC <65678461+AndyLeUNCC@users.noreply.github.com>
2021-10-26 15:58:18 -0400

    add the push notifications testing
    This is a push notifications as a testing. It is not real feature.

--- app/src/main/java/com/example/rush/MainActivity.java
+++ app/src/main/java/com/example/rush/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.rush;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
@@ -19,6 +25,34 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
         setContentView(R.layout.activity_main);
         //Navigation bar
         bottomNavigation();
+
+        //Push notifications
+        pushNotifications();
+    }
+
+    /*
+    * The Push Notification feature now is implementing as a testing.
+    * This feature will be integrated into this Rush app.
+    * */
+    public void pushNotifications(){
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg =  token;
+                        Log.d("Token", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 
