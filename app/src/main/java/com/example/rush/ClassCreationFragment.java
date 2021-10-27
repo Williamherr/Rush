507d8a4f48543b9896d1c8bcb07fcb22fe006891
Taylor Thomas <tthom119@uncc.edu>
2021-10-27 12:35:40 -0400

    Added logic for reading classes for logged in user

--- app/src/main/java/com/example/rush/ClassCreationFragment.java
+++ app/src/main/java/com/example/rush/ClassCreationFragment.java
@@ -1,11 +1,8 @@
 package com.example.rush;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/example/rush/ClassesFragment.java
+++ app/src/main/java/com/example/rush/ClassesFragment.java
@@ -1,17 +1,26 @@
 package com.example.rush;
 
-import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ClassesFragment extends Fragment {
@@ -19,10 +28,24 @@ public class ClassesFragment extends Fragment {
     FloatingActionButton fabButton, fabButton2, fabButton3;
     Animation fabOpen, fabClose, rotateForward, rotateBackward;
     boolean isOpen = false;
+    private FirebaseDatabase database;
+    private DatabaseReference dbRef;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private TextView noClasses;
+    String userID;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        //Check if user is not logged in
+        if (user != null) {
+            userID = user.getUid();
+        }
 
     }
 
@@ -38,6 +61,7 @@ public class ClassesFragment extends Fragment {
         fabButton = view.findViewById(R.id.classOptionsButton);
         fabButton2 = view.findViewById(R.id.classDeleteButton);
         fabButton3 = view.findViewById(R.id.classEditButton);
+        noClasses = view.findViewById(R.id.classesText);
         fabOpen = AnimationUtils.loadAnimation
                 (getActivity(), R.anim.fab_open);
         fabClose = AnimationUtils.loadAnimation
@@ -69,11 +93,38 @@ public class ClassesFragment extends Fragment {
 
             }
         });
+        //Check if user is not null
+        if (userID != null) {
+            dbRef.child(userID).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    ClassInfo tempClassInfo = snapshot.getValue(ClassInfo.class);
+                    //If tempClassInfo is null, there are no classes
+                    if (tempClassInfo != null) {
+                        Log.d("Name: ", tempClassInfo.getClassName() + "Instructor: " +
+                                tempClassInfo.getInstructor());
+                    } else {
+                        noClasses.setText("No classes to display.");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.w("Failed to read value.", error.toException());
+
+                }
+            });
+        } else {
+            /* User shouldn't be able to access this tab when logged out
+               Mostly used for testing */
+            noClasses.setText("You are not logged in.");
+        }
 
 
         return view;
     }
 
+    //This method is used to animate the floating action buttons when user clicks on it
     private void animateFab() {
         if (isOpen) {
             fabButton.startAnimation(rotateForward);
