24452d5792cc76a1b3c63dca05e2a3e60cfc601f
Taylor Thomas <tthom119@uncc.edu>
2021-10-27 22:50:57 -0400

    Added RecyclerView to ClassesFragment so that users can see their classes

--- app/src/main/java/com/example/rush/ClassesFragment.java
+++ app/src/main/java/com/example/rush/ClassesFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,34 +15,39 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 
 public class ClassesFragment extends Fragment {
 
     FloatingActionButton fabButton, fabButton2, fabButton3;
-    Animation fabOpen, fabClose, rotateForward, rotateBackward;
+    Animation fabOpen, fabClose;
     boolean isOpen = false;
-    private FirebaseDatabase database;
-    private DatabaseReference dbRef;
+    private FirebaseFirestore database;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private TextView noClasses;
+    private RecyclerView classView;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<ClassInfo> listOfClasses = new ArrayList<>();
     String userID;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        database = FirebaseDatabase.getInstance();
-        dbRef = database.getReference();
+        database = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         //Check if user is not logged in
@@ -55,21 +63,28 @@ public class ClassesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_classes, container, false);
         /*
+            This section initializes the RecyclerView to show the user's classes
+         */
+        RecyclerView recycle = (RecyclerView) view.findViewById(R.id.classes);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getActivity());
+        recycle.setLayoutManager(manager);
+        recycle.addItemDecoration(new DividerItemDecoration(getActivity(),
+                DividerItemDecoration.VERTICAL));
+        /*
             This section is used for the FloatingActionButton menu for creating/joining a class
             and deleting classes
          */
         fabButton = view.findViewById(R.id.classOptionsButton);
         fabButton2 = view.findViewById(R.id.classDeleteButton);
         fabButton3 = view.findViewById(R.id.classEditButton);
-        noClasses = view.findViewById(R.id.classesText);
+        noClasses = view.findViewById(R.id.noClassesText);
         fabOpen = AnimationUtils.loadAnimation
                 (getActivity(), R.anim.fab_open);
         fabClose = AnimationUtils.loadAnimation
                 (getActivity(), R.anim.fab_close);
-        rotateForward = AnimationUtils.loadAnimation
-                (getActivity(), R.anim.rotate_forward);
-        rotateBackward = AnimationUtils.loadAnimation
-                (getActivity(), R.anim.rotate_backward);
+        /*
+            The below sets up the onClickListener for each button
+         */
 
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,25 +110,36 @@ public class ClassesFragment extends Fragment {
         });
         //Check if user is not null
         if (userID != null) {
-            dbRef.child(userID).addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    ClassInfo tempClassInfo = snapshot.getValue(ClassInfo.class);
-                    //If tempClassInfo is null, there are no classes
-                    if (tempClassInfo != null) {
-                        Log.d("Name: ", tempClassInfo.getClassName() + "Instructor: " +
-                                tempClassInfo.getInstructor());
-                    } else {
-                        noClasses.setText(R.string.no_classes);
-                    }
-                }
+            //Runs a query for any classes created by the current user
+            database.collection("classes")
+                    .whereEqualTo("createdBy", userID)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("Success", document.getId() + " => " + document.getData());
+                                    //Cast the QueryDocumentSnapshot into a ClassInfo obj
+                                    try {
+                                        ClassInfo obj = document.toObject(ClassInfo.class);
+                                        //Keep track of all classes created by this user
+                                        listOfClasses.add(obj);
+                                        adapter = new ClassAdapter(listOfClasses);
+                                        recycle.setAdapter(adapter);
+                                    } catch (NullPointerException e) {
+                                        Toast.makeText(getActivity(), "Sorry, something went wrong!"
+                                                , Toast.LENGTH_SHORT).show();
+                                    }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Log.w("Failed to read value.", error.toException());
+                                }
+                            } else {
+                                Log.d("Error", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
 
-                }
-            });
+
         } else {
             /* User shouldn't be able to access this tab when logged out
                Mostly used for testing */
@@ -127,14 +153,12 @@ public class ClassesFragment extends Fragment {
     //This method is used to animate the floating action buttons when user clicks on it
     private void animateFab() {
         if (isOpen) {
-            fabButton.startAnimation(rotateForward);
             fabButton2.startAnimation(fabClose);
             fabButton2.setClickable(false);
             fabButton3.startAnimation(fabClose);
             fabButton3.setClickable(false);
             isOpen = false;
         } else {
-            fabButton.startAnimation(rotateBackward);
             fabButton2.startAnimation(fabOpen);
             fabButton2.setClickable(true);
             fabButton3.startAnimation(fabOpen);
@@ -142,4 +166,5 @@ public class ClassesFragment extends Fragment {
             isOpen = true;
         }
     }
+
 }
