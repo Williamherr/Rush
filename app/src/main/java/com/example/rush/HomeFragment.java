8c7738d2d9daa16a7e645c79ae05e6eff535172d
William Herr <herrwilliam@outlook.com>
2021-11-01 20:41:13 -0400

    Fixed instant messaging and added menus to the messages: Report, Delete, and Edit

--- app/src/main/java/com/example/rush/HomeFragment.java
+++ app/src/main/java/com/example/rush/HomeFragment.java
@@ -7,16 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class HomeFragment extends Fragment {
 
+
+
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
-    TextView textView;
 
 
     @Override
@@ -30,8 +30,6 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        textView = view.findViewById(R.id.Home);
-
         return view;
     }
 }
