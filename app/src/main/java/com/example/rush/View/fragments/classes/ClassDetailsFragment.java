a00cfb54453184d2e33087b5583d242ee6ca2009
Taylor Thomas <tthom119@uncc.edu>
2021-11-27 17:02:21 -0500

    Added icon to denote professor in list of class members

--- app/src/main/java/com/example/rush/View/fragments/classes/ClassDetailsFragment.java
+++ app/src/main/java/com/example/rush/View/fragments/classes/ClassDetailsFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.rush.Model.ClassInfo;
@@ -31,7 +32,7 @@ import org.w3c.dom.Document;
 import java.util.ArrayList;
 
 public class ClassDetailsFragment extends Fragment {
-    private String name, instructor, description, id, userID;
+    private String name, instructor, description, id, userID, professor, professorID;
     private FirebaseFirestore database;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
@@ -74,6 +75,21 @@ public class ClassDetailsFragment extends Fragment {
         recycle.setLayoutManager(manager);
         recycle.addItemDecoration(new DividerItemDecoration(getActivity(),
                 DividerItemDecoration.VERTICAL));
+        database.collection("classes").document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    ClassInfo obj = doc.toObject(ClassInfo.class);
+                    professor = obj.getInstructor();
+                    professorID = obj.getCreatedBy();
+                    Member p = new Member(professor, professorID);
+                    students.add(p);
+                    adapter = new DetailsAdapter(students);
+                    recycle.setAdapter(adapter);
+                }
+            }
+        });
 
         database.collection("classes").document(id).collection("Students").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -118,6 +134,12 @@ public class ClassDetailsFragment extends Fragment {
             Member obj = students.get(position);
             holder.studentName.setText(obj.getName());
 
+            if (obj.getName().equals(professor) && obj.getUid().equals(professorID)) {
+                holder.status.setVisibility(View.VISIBLE);
+            } else {
+                holder.status.setVisibility(View.INVISIBLE);
+            }
+
         }
 
         @Override
@@ -132,12 +154,14 @@ public class ClassDetailsFragment extends Fragment {
         public class ViewHolder extends RecyclerView.ViewHolder {
             private View view;
             private TextView studentName;
+            private ImageView status;
 
 
             public ViewHolder(View view) {
                 super(view);
                 this.view = view;
                 studentName = view.findViewById(R.id.nameOfStudent);
+                status = view.findViewById(R.id.professorStatus);
             }
         }
     }
